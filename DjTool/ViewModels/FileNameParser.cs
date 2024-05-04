b9c7e47962deff53118122b24044180ab42ce92a
Roman Gorgol <roman.gorgol@gmail.com>
2024-05-04 17:07:59 +0500

    парсер

---
+++ DjTool/ViewModels/FileNameParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DjTool.ViewModels
+{
+    internal class FileNameParser
+    {
+        private Regex regex = new Regex(@"^(?<order>\d+).\s+(?<name>.+)$");
+
+        public FileNameParser() { }
+
+        public TodoItemViewModel ParseFileName(string filepath)
+        {
+            var filename = System.IO.Path.GetFileName(filepath);
+
+            var match = regex.Match(filename);
+            if (match.Success)
+            {
+                var order = match.Groups["order"].Success
+                    ? (int?)int.Parse(match.Groups["order"].Value)
+                    : null;
+
+                var name = match.Groups["name"].Value;
+                return new TodoItemViewModel(name, filepath, order);
+            }
+            else
+            {
+                return new TodoItemViewModel(filename, filepath, null);
+            }
+        }
+
+    }
+}
